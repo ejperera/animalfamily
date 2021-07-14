962f3d567aab97651819c0b7bec5f6cbdba53caf
CCS\perer <eric.perera@champ.aero>
2021-07-15 07:26:54 +0800

    First commit

---
+++ src/main/java/com/animalfamily/AnimalfamilyApplication.java
@@ -0,0 +1,13 @@
+package com.animalfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalfamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalfamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animalfamily/AnimalfamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalfamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalfamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
