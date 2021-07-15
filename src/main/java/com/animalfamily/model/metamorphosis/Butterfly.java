c4045eaa47a36316e2957098f964c4e1f66c5a6c
CCS\perer <eric.perera@champ.aero>
2021-07-15 08:25:47 +0800

    C.1. Can you model a butterfly?

---
+++ src/main/java/com/animalfamily/model/metamorphosis/Butterfly.java
@@ -0,0 +1,8 @@
+package com.animalfamily.model.metamorphosis;
+
+import com.animalfamily.model.Animal;
+import com.animalfamily.model.Flyable;
+import com.animalfamily.model.Walkable;
+
+public class Butterfly  extends Animal implements Flyable, Walkable {
+}
---
+++ src/test/java/com/animalfamily/model/metamorphosis/ButterflyTest.java
@@ -0,0 +1,16 @@
+package com.animalfamily.model.metamorphosis;
+
+import com.animalfamily.model.fish.Shark;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ButterflyTest {
+
+    @Test
+    public void testButterfly(){
+
+        Butterfly butterfly = new Butterfly();
+        Assertions.assertEquals(butterfly.fly(), "I am flying");
+        Assertions.assertEquals(butterfly.walk(), "I am walking");
+    }
+}
